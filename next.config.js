da7a9a6bdb2942c770da4b9aca0ee22c6a8a599d
ThanhLe <nhatthanh5891@gmail.com>
2021-03-02 18:07:30 +0700

    add amplify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const { determineBuildId } = require('./build-id')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer()
+module.exports = withBundleAnalyzer({
+  generateBuildId: async () => {
+    const buildId = await determineBuildId()
+    console.log(`> Build ID: ${buildId}`)
+    return buildId
+  },
+})
