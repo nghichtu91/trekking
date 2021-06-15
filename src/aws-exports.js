e7cde56e7c4fe0d39359deda2a38af3d79b185d5
nghichtu91 <nhatthanh5891@gmail.com>
2021-06-15 09:16:45 +0700

    amplify

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,12 +3,12 @@
 
 const awsmobile = {
     "aws_project_region": "ap-southeast-1",
-    "aws_cognito_identity_pool_id": "ap-southeast-1:defad508-d6a4-4d91-8986-2d38fa46002e",
+    "aws_cognito_identity_pool_id": "ap-southeast-1:db0e0c5b-1b3b-476e-a369-3e7eb0188fb4",
     "aws_cognito_region": "ap-southeast-1",
-    "aws_user_pools_id": "ap-southeast-1_Mvqw1dYd6",
-    "aws_user_pools_web_client_id": "7cgcb29nvdladcfq69hmhj119l",
+    "aws_user_pools_id": "ap-southeast-1_PdK5F8gnS",
+    "aws_user_pools_web_client_id": "3uqvoeh18tedhjdlkep98nv6ep",
     "oauth": {
-        "domain": "trekking73950f92-73950f92-dev.auth.ap-southeast-1.amazoncognito.com",
+        "domain": "trekking73950f92-73950f92-test.auth.ap-southeast-1.amazoncognito.com",
         "scope": [
             "phone",
             "email",
@@ -16,12 +16,12 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://test.leonui.app/",
+        "redirectSignOut": "https://test.leonui.app/",
         "responseType": "token"
     },
     "federationTarget": "COGNITO_USER_AND_IDENTITY_POOLS",
-    "aws_user_files_s3_bucket": "trekking0a3270a152dc4cb0b5ba6e3dcb758607112657-dev",
+    "aws_user_files_s3_bucket": "trekking0a3270a152dc4cb0b5ba6e3dcb75860775048-test",
     "aws_user_files_s3_bucket_region": "ap-southeast-1"
 };
 
